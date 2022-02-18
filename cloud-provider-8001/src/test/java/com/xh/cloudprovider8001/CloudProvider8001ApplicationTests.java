f68a111db55f2803d9be3b6ab582c8ce81a626b1
xiaohong <345125740@qq.com>
2022-02-18 18:22:31 +0800

    redisson

--- cloud-provider-8001/src/test/java/com/xh/cloudprovider8001/CloudProvider8001ApplicationTests.java
+++ cloud-provider-8001/src/test/java/com/xh/cloudprovider8001/CloudProvider8001ApplicationTests.java
@@ -1,11 +1,15 @@
 package com.xh.cloudprovider8001;
 
+import com.xh.cloudprovider8001.redission.designresource.RedisDesignResourceSnGenerator;
 import com.xh.cloudprovider8001.service.KafkaOutService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {CloudProvider8001Application.class})
 class CloudProvider8001ApplicationTests {
 
     @Autowired
@@ -15,6 +19,27 @@ class CloudProvider8001ApplicationTests {
     void contextLoads() {
         for (int i = 0; i < 100000000; i++) {
             kafkaOutService.sayHello(i+"");
+        }
+    }
+
+    @Autowired
+    private RedisDesignResourceSnGenerator redisDesignResourceSnGenerator;
+
+    @Test
+    public void create() {
+        for (int i = 0; i < 20; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                for (int j = 0; j < 30; j++) {
+                    String sequence = redisDesignResourceSnGenerator.getSn("ADT" + finalI, false);
+                    System.out.println("Thread = " + Thread.currentThread().getId() + " --> seq = " + sequence);
+                }
+            }).start();
+        }
+        try {
+            Thread.sleep(10000 * 100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
